be02d800e1636c5ec2a108b260553b351a007c30
EwenHawk <nathangellenoncourt@gmail.com>
2025-07-30 17:26:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 
 # 🆔 Récupération de l'ID_Panneau depuis l'URL
+id_panneau = st.query_params.get("id_panneau", [""])[0]
 st.subheader(f"🆔 ID du panneau : {id_panneau}")
 TARGET_KEYS = ["Voc", "Isc", "Pmax", "Vpm", "Ipm"]
 
