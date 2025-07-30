3ffe7122c779b1c63cf3b565748794cf3e255b1a
EwenHawk <nathangellenoncourt@gmail.com>
2025-07-30 17:27:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from google.oauth2.service_account import Credentials
 
 # 🆔 Récupération de l'ID_Panneau depuis l'URL
 id_panneau = st.query_params.get("id_panneau", [""])[0]
-st.subheader(f"🆔 ID du panneau : {id_panneau}")
+st.info(f"🆔 ID détecté : `{id_panneau}`")
 TARGET_KEYS = ["Voc", "Isc", "Pmax", "Vpm", "Ipm"]
 
 # États Streamlit
